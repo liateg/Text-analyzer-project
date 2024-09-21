926430e6eb98ccb482ba5c93f0c9996d50ce4597
Liya Tegared <liyategared7@gmail.com>
2024-09-21 18:43:37 +0300

    Fainal submitted code

--- text_analyzer.py
+++ text_analyzer.py
@@ -6,7 +6,7 @@ import nltk  # For natural language processing tasks
 
 # Downloading necessary NLTK data
 nltk.download('punkt')  # Tokenizer models
-nltk.download('wordnet')  # WordNet lexical database
+#nltk.download('wordnet')  # WordNet lexical database
 nltk.download("stopwords")  # Stopwords list
 
 # Importing specific functions from NLTK
@@ -33,7 +33,7 @@ def load_data(file_path):
     DataFrame: The loaded data.
     """
     try:
-        return pd.read_fwf(file_path)
+        return pd.read_fwf(os.path.abspath(file_path))
     except FileNotFoundError as e:
         print(f"Error: {e}")
         print("Please check the file path and ensure the file exists.")
@@ -160,9 +160,36 @@ def count_alphabet_frequency(alphabets):
             counter[char] = alphabets.count(char)
     return counter
 
+#Write Geeze charachters
+def Title_write(text):
+    op=open('Output.doc',mode='a')
+    op.write(text)
+    op.close()
+    
+    
+def list_Geez_write(token):
+    with open ('Output.doc', encoding='utf-8', mode='a') as report_file:
+       
+        for word in token:
+            report_file.write(f'{word} ')
+
+def dect_Geez_write(counter):
+ 
+    with open ('Output.doc', encoding='utf-8', mode='a') as report_file:
+        for key,value in counter.items() :
+
+            report_file.write(f'{key}:{value} ')
+
+def overllaping_calc(counter1,counter2):
+
+    overllaped_phonem=set.intersection(set(counter2),set(counter1))
+    Union=set.union(set(counter1),set(counter2))
+
+    return (len(overllaped_phonem)/len(Union))*100  
+
 # Load the data
-dfa = load_data('.\Amharic.txt')
-dft = load_data('.\Tegrigna.txt')
+dfa = load_data('C:\\Users\\Liya\\Desktop\\5k\\Project\\Amharic.txt')
+dft = load_data('C:\\Users\\Liya\\Desktop\\5k\\Project\\Tegrigna.txt')
 
 # Clean the data
 dfa = clean_data(dfa, "Amharic")
@@ -176,27 +203,13 @@ dft_t = tokenize_data(dft, "Tigregna")
 dfa_t = remove_stopwords(dfa_t, 'amharic')
 dft_t = remove_stopwords(dft_t, 'tigregna')
 
-# Print the cleaned data
-
-print(f'The preprocessed Amharic text: \n\n {dfa_t}\n')
-print(f'The preprocessed Tigrigna text: \n\n {dft_t}\n')
-
 # Count word frequency
 acounter = count_frequency(dfa_t)
 tcounter = count_frequency(dft_t)
 
-# Print word frequency
-print(f'The word frequency in the given Tigrigna and Amharic text is shown below \n Tigrigna \n\n{tcounter}')
-print('Amharic \n\n',acounter)
-
 # Get unique words
 aunique_word = get_unique_words(acounter)
 tunique_word = get_unique_words(tcounter)
-
-# Print unique words
-print(f'Unique words for the given Tigrigna text are\n\n {tunique_word}\n')
-print(f'Unique words for the given Amharic text are\n \n{aunique_word}\n')
-
 
 # Extract alphabets
 aalphabet = extract_alphabets(acounter)
@@ -206,42 +219,73 @@ talphabet = extract_alphabets(tcounter)
 aalphabet_counter = count_alphabet_frequency(aalphabet)
 talphabet_counter = count_alphabet_frequency(talphabet)
 
-# Print alphabet frequency
-print(f'Alphabetical frequency in the given Amharic text: \n\n {aalphabet_counter}\n')
-print(f'Alphabetical frequency in the given Tigrigna text: \n\n {talphabet_counter}\n')
-
 #Clculate word and alphabet level overlap
 # Find overlapping words
 overlapped_word =get_overlapping_words(acounter, tcounter)
-# Print overlapping words
-print(f'The number of words that are in both Amharic and Tigrigna texts are\n {len(overlapped_word)} \nHere are the words\n\n {overlapped_word}\n')
+
 #Find overlapping Alphabets
 overlapped_alphabet= get_overlapping_words(talphabet_counter, aalphabet_counter)
-print(f'The number of letters in both Amharic and Tigrigna texts is\n {len(overlapped_alphabet)} \nHere are the letters\n \n{overlapped_alphabet}\n')
+
 # Converting Ethiopic alphabets into their phonetic representations
 aipa_converted_alphabet = ipa_convertor_alpahbet(aalphabet)  # alphabet level
 tipa_converted_alphabet = ipa_convertor_alpahbet(talphabet)
 
 aipa_converted_df = ipa_convert(dfa_t)  # Text level
 tipa_converted_df = ipa_convert(dft_t)
+# Print the cleaned data
+op=open('Output.doc','w')
+op.write('This is all the output of the code\n')
+op.close()
+         
 
-#Print the  phoneme of the given text
-print(f'The phoenem of the given Amharic text\n')
+Title_write('The preprocessed Amharic text:\n')
+list_Geez_write(dfa_t)
+
+Title_write('The preprocessed Tigregna text:\n')
+list_Geez_write(dft_t)
+
+Title_write('The word frequency in the given Tigrigna and Amharic text is shown below\nTigregna\n')
+dect_Geez_write(tcounter)
+Title_write('Amharic')
+dect_Geez_write(acounter)
+Title_write('Unique words for the given Tigrigna text are\n')
+Title_write('Total number of unique words in the Tigrigna text:  ')
+Title_write(str(len(tunique_word)))
+list_Geez_write(tunique_word)
+Title_write('Unique words for the given Amharic text are\n')
+Title_write('Total number of unique words in the Amharic text:  ')
+Title_write(str(len(aunique_word)))
+list_Geez_write(aunique_word)
+
+Title_write('Alphabetical frequency in the given Amharic text: \n')
+dect_Geez_write(aalphabet_counter)
+Title_write('Alphabetical frequency in the given Tigregna text: \n')
+dect_Geez_write(talphabet_counter)
+Title_write('The number of words that are in both Amharic and Tigrigna texts are: ')
+Title_write(str(len(overlapped_word)))
+Title_write('Here are the words\n')
+list_Geez_write(overlapped_word)
+Title_write('The number of alphabets that are in both Amharic and Tigrigna texts are: ')
+Title_write(str(len(overlapped_alphabet)))
+Title_write('Here are the words\n')
+list_Geez_write(overlapped_alphabet)
+
+# #Print the  phoneme of the given text
+Title_write('\nThe phoenem of the given Amharic text\n')
 aipa_converted_alphabet_single=[]
 for word in aipa_converted_df:
-    print(word,'',end='')
+    op=open('Output.doc',encoding='utf-8',mode='a')
+    op.write(word)
     for char in word:
         aipa_converted_alphabet_single.append(char)
-print()
 
-print(f'The phoenem of the given Tigregna text\n')
-
+Title_write('\nThe phoenem of the given Tigregna text\n')
 tipa_converted_alphabet_single=[]
 for word in tipa_converted_df:
-    print(word,'',end='')
+    op=open('Output.doc',encoding='utf-8',mode='a')
+    op.write(word)
     for char in word:
         tipa_converted_alphabet_single.append(char)
-
    
 # Counting the frequency of each phonetic character
 aipa_converted_alphabet_counter = {}
@@ -255,14 +299,35 @@ for char in tipa_converted_alphabet:
         tipa_converted_alphabet_counter[char] = tipa_converted_alphabet.count(char)
 
 #Print the alphabet  phoneme frequancy
-print()
-print(f'Alphabetical  phoneme frequency in the given Amharic text: \n\n {aipa_converted_alphabet_counter}\n')
-print(f'Alphabetical  phoneme  frequency in the given Tigrigna text: \n \n{tipa_converted_alphabet_counter}\n')
+#print()
+Title_write('\n\nAlphabetical  phoneme frequency in the given Amharic text: \n') 
+dect_Geez_write(aipa_converted_alphabet_counter)
 
+Title_write('\n\nAlphabetical  phoneme frequency in the given Tigregna text: \n') 
+dect_Geez_write(tipa_converted_alphabet_counter)
+#print(f'Alphabetical  phoneme frequency in the given Tigrigna text: \n \n{tipa_converted_alphabet_counter}\n')
 
 aipa_converted_single_alphabet_counter = count_alphabet_frequency(aipa_converted_alphabet_single)
-
 tipa_converted_single_alphabet_counter = count_alphabet_frequency(tipa_converted_alphabet_single)
 
-print(f'Single phoneme  frequency in the given Amharic text: \n\n{aipa_converted_single_alphabet_counter}\n')
-print(f'Single phoneme  frequency in the given Tigrigna text: \n\n{tipa_converted_single_alphabet_counter}')
+#print(f'Single phoneme  frequency in the given Amharic text: \n\n{aipa_converted_single_alphabet_counter}\n')
+Title_write('\n\nSingle phoneme  frequency in the given Amharic text: \n')
+dect_Geez_write(aipa_converted_single_alphabet_counter)
+#print(f'Single phoneme  frequency in the given Tigrigna text: \n\n{tipa_converted_single_alphabet_counter}')
+Title_write('\n\nSingle phoneme  frequency in the given Tigregna text: \n')
+dect_Geez_write(tipa_converted_single_alphabet_counter)
+#Phonetic overllaping analysis-character level 
+
+#Phonetic overllaping analysis-phonem level 
+single_phonem_overllaped=set.intersection(set(aipa_converted_single_alphabet_counter),set(tipa_converted_single_alphabet_counter))
+vowels={'a','e','o','ə', 'u','i', 'ɨ','ɒ','ʌ','æ','ɔ','ɜ'}
+single_phonem_overllaped.difference_update(vowels)  #Remove Vowels-> short vowels: /ɪ/ /ʊ/ /ə/ /e/ /ɒ/ /ʌ/ /æ/ 5 long vowels: /iː/ /uː/ /ɑː/ /ɔː/ /ɜː/
+Union=set.union(set(tipa_converted_single_alphabet_counter),set(aipa_converted_single_alphabet_counter))
+Union.difference_update(vowels)
+single_phonem_overllaped_phonem=len(single_phonem_overllaped)/len(Union)*100
+
+#Analysis
+Title_write(f'\n\nAnalysis\nWord level overlaping in percentache is: {overllaping_calc(acounter, tcounter)}%\nCharacter level overlaping in percentache is: {overllaping_calc(aalphabet, talphabet)}%\nCombined Phonem level overlap in percentage is: {overllaping_calc(tipa_converted_alphabet_counter, aipa_converted_alphabet_counter)}%\n Phonem level overlap is: {single_phonem_overllaped_phonem}%\n--------END------------')
+
+
+
